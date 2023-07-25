0f6f2c0bd68921306dec528a79856d53d15201d2
BorislavJivkov09 <zivkov588@gmail.com>
2023-07-25 19:41:40 +0300

    Solved problem with conditional structure.

---
+++ ConditionalStructures/ConditionalStructures/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace PurviOpit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //да се изчете от конзолата сметката на ани
+            //ако има отстъпка трябва сметката да е по голяма или равна на 10.50
+            //ако има отстъпка трябва да изпише Console.WriteLine("Ще имате отстъпка") ако не ("Еми.. шанс")
+            double smetka = double.Parse(Console.ReadLine());
+            double smetkaOtstupka = 10.50;
+            if (smetka >= smetkaOtstupka)
+            {
+                Console.WriteLine("Ще имате отстъпка");
+            }
+
+            if (smetka < smetkaOtstupka)
+            {
+                Console.WriteLine("Еми.. шанс");
+            }
+        }
+    }
+}
