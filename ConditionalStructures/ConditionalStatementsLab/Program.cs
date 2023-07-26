3ca3b6f9495050fc7a6717f0485409b4bdcaf2d5
BorislavJivkov09 <zivkov588@gmail.com>
2023-07-26 20:32:43 +0300

    Solved ExcellentResult

---
+++ ConditionalStructures/ConditionalStatementsLab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConditionalStatementsLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //от конзолата четем оценка
+
+            double grade = double.Parse(Console.ReadLine());
+
+            //проверяваме оценката и изписваме резултата
+            // ако е 5.50 или по-голяма изписваме Excellent
+
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
