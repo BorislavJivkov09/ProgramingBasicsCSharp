070251547a9ff85be3630bb85990e405b0d27595
BorislavJivkov09 <zivkov588@gmail.com>
2023-07-26 20:55:49 +0300

    Solved GreaterNumber

---
+++ ConditionalStructures/BiggerNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BiggerNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Четем две цели числа от конзолата
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            //Сравняваме ги и изписваме по-голямото
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else 
+            {
+                Console.WriteLine(num2);
+            }
+        }
+    }
+}
