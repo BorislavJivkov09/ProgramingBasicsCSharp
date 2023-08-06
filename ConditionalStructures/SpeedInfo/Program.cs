baaa44f8f8d57e8a0eb7663ecd60160a12fbac5c
BorislavJivkov09 <zivkov588@gmail.com>
2023-08-06 20:37:43 +0300

    Solved SpeedInfo

---
+++ ConditionalStructures/SpeedInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SpeedInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // трябва да се напише програма която чете скорост с реално число
+            double number = double.Parse(Console.ReadLine());
+            // при скорост до 10 slow 
+            if (number <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (number <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if (number <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (number <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else if (number > 1000)
+            {
+                Console.WriteLine("extremely fast");
+            }
+
+
+        }
+    }
+}
