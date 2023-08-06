d94c070b4b20ba72f3b09507788ebf1cd8663234
BorislavJivkov09 <zivkov588@gmail.com>
2023-08-06 20:04:59 +0300

    Solved PasswordGuess

---
+++ ConditionalStructures/PasswordGuess/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PasswordGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //трябва да се направи програма която да чете парола
+            string password = Console.ReadLine();
+            string correct = "s3cr3t!P@ssw0rd";
+
+            //ако е вярна да се изпише welcome ако е грешна да изпишем wrong password
+            if (password == correct)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
